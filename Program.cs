eaac0c2e11c68e822f717be57ab9b8b8ca44054f
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-08-09 20:35:26 -0600

    Area
    Will calculate the area of a rectangle, circle, and triangle.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace ArchitectArithmetic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(RectangleArea(4,5));
+            Console.WriteLine(CircleArea(4));
+            Console.WriteLine(TriangleArea(10,9));
+        }
+
+        static double RectangleArea( double length, double width)
+        {
+            return length * width;
+        }
+
+        static double CircleArea(double radius)
+        {
+            double pi = Math.PI;
+            return pi * Math.Pow(radius, 2);
+        }
+
+        static double TriangleArea(double bottom, double height)
+        {
+            return .5 * bottom * height;
         }
     }
 }
